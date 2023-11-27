627a6b36c06484e615b0251d36ed0050eb452bb0
Ambroise BAZIE <ambroise.bazie@infisoftware.net>
2023-11-27 01:45:32 +0100

    feat(core/button): Update button with appearance property

--- examples/src/app/app.component.ts
+++ examples/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { ButtonComponent } from '@ognanshissi/core';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, ButtonComponent],
+  imports: [RouterModule, ButtonComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
--- packages/core/src/lib/core/ui/button/button.component.ts
+++ packages/core/src/lib/core/ui/button/button.component.ts
@@ -1,5 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, HostBinding, Input, OnInit, booleanAttribute } from '@angular/core';
+import { ButtonAppearance } from './button.type';
+
+
 
 @Component({
   selector: 'button[filled-button], button[raised-button], button[outlined-button]',
@@ -13,9 +16,21 @@ export class ButtonComponent implements OnInit {
 
   @Input({transform: booleanAttribute}) isLoading!: boolean;
 
+  @Input() appearance: ButtonAppearance = 'default';
+
   @HostBinding('attr.role') role = 'button';
 
   constructor() { }
+
+  @HostBinding('class')
+  get classes() {
+    return {
+      'core-button-primary': this.appearance == 'primary',
+      'core-button-default': this.appearance == 'default',
+      'core-button-accent': this.appearance == 'accent',
+      'core-button-warn': this.appearance == 'warn'
+    }
+  }
 
   ngOnInit() {
     console.log("From custom button");
